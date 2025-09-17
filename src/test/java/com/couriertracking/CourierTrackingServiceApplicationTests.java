276c42ca46741a1517fdda3b1796b87d886f5917
gokhanaker <gokhanaker@gmail.com>
2025-09-18 00:29:22 +0300

    feat: initialized repo

---
+++ src/test/java/com/couriertracking/CourierTrackingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.couriertracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourierTrackingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
