ecfb1b1d1b4b9175afcb8a8eabf7c230a4411292
gokhanaker <gokhanaker@gmail.com>
2025-09-18 13:41:46 +0300

    feat: added Location entity

---
+++ src/main/java/com/couriertracking/model/Location.java
@@ -0,0 +1,44 @@
+package com.couriertracking.model;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.UuidGenerator;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "locations")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Location {
+    
+    @Id
+    @UuidGenerator
+    @Column(columnDefinition = "UUID", updatable = false, nullable = false)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "courier_id", nullable = false)
+    private Courier courier;
+
+    @Column(nullable = false)
+    private Double latitude;
+
+    @Column(nullable = false)
+    private Double longitude;
+
+    @Column(nullable = false)
+    private LocalDateTime timestamp;
+}
